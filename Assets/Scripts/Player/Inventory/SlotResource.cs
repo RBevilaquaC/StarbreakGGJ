5c2b29d7326600c5d32e68c5393f1cf5ddbc43f4
Roberto Bevilaqua <rbdc.eng@uea.edu.br>
2023-01-31 19:55:18 -0400

    Init inventory

---
+++ Assets/Scripts/Player/Inventory/SlotResource.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Cecil;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotResource : MonoBehaviour
+{
+    #region Parameters
+
+    public bool haveResource;
+    public bool fullStack;
+    public int type;
+    public int CurrentResource;
+
+    private TMP_Text textAmount;
+    private Image imageIcon;
+
+    #endregion
+
+    private void Start()
+    {
+        textAmount = transform.GetChild(0).GetComponent<TMP_Text>();
+        imageIcon = transform.GetChild(1).GetComponent<Image>();
+    }
+
+    public void UpdateUI()
+    {
+        imageIcon.enabled = true;
+        textAmount.text = CurrentResource.ToString();
+        if (!haveResource)
+        {
+            imageIcon.enabled = false;
+            textAmount.text = "";
+        }
+    }
+
+    public void UpdateIcon(Sprite newSprite)
+    {
+        imageIcon.sprite = newSprite;
+    }
+}
