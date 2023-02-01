1b42a12a8f57a0035d0176b664646deea7e5db65
Roberto Bevilaqua <rbdc.eng@uea.edu.br>
2023-02-01 13:59:14 -0400

    GameOverPanel

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLife : LifeSystem
 {
-    
+    #region Parameters
+
+    [SerializeField] private Slider lifeBar;
+    [SerializeField] private GameObject gameOverPanel;
+    #endregion
+
+    protected override void Heal(int healAmount)
+    {
+        base.Heal(healAmount);
+        UpdateLifeBar();
+    }
+
+    protected override void Death()
+    {
+        base.Death();
+        gameOverPanel.SetActive(true);
+    }
+
+    public override void TakeDamage(int damageAmount)
+    {
+        currentLife -= damageAmount;
+        if (currentLife <= 0)
+        {
+            currentLife = 0;
+            UpdateLifeBar();
+            Death();
+        }
+        UpdateLifeBar();
+    }
+
+    private void UpdateLifeBar()
+    {
+        lifeBar.value = (float)currentLife / maxLife;
+    }
 }
