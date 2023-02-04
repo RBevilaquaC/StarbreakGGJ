3172b509525516985fea2a49029af80fe9226295
Roberto Bevilaqua <rbdc.eng@uea.edu.br>
2023-02-04 18:11:14 -0400

    Player Attack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
@@ -14,6 +15,35 @@ public class PlayerAttack : MonoBehaviour
 
     private void Start()
     {
-        throw new NotImplementedException();
+        anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !PlayerStatus.status.isAttacking)
+        {
+            PlayerStatus.status.isAttacking = true;
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        anim.Play("Attack");
+        Invoke("DisableAttack",0.3f);
+    }
+
+    private void DisableAttack()
+    {
+        PlayerStatus.status.isAttacking = false;
+    }
+
+    
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.GetComponent<LifeSystem>() != null && col.GetComponent<PlayerStatus>() == null && PlayerStatus.status.isAttacking)
+        {
+            col.GetComponent<LifeSystem>().TakeDamage(damageAttack);
+        }
     }
 }
